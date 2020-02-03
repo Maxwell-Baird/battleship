ed65c087a994ddae2f79f436111736d515e4dd9c
Maxwell Baird <bairdm71@gmail.com>
2020-02-03 14:47:10 -0700

    changed the test to check length instead of health

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,9 +7,9 @@ class ShipTest
     assert_instance_of Ship, cruiser
   end
 
-  def test_ship_has_health
+  def test_ship_has_length
     cruiser = Ship.new("Cruiser", 3)
-    assert_equal 3, cruiser.health
+    assert_equal 3, cruiser.length
   end
 
 end
