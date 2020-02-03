2ba190f6a2ecd71f6d92afa8a2c52cabbc1843a6
“Krista <stadler.krista@gmail..com>
2020-02-03 16:57:53 -0700

    Wrote name test and health test

---
+++ test/ship_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship.rb
+
+class TurnTest < Minitest::Test
+
+#Test Ship Exists --> Max
+
+  def test_ship_has_name
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", ship.name
+  end
+
+#Test Ship Has Length --> Max
+
+  def test_ship_health
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.health
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+  end    
+
+#Assigned Tests
+#Health --> Krista
+#Sunk --> Max
+#Hit --> Krista
