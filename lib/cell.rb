abafa7166bf0210105617b7e6ecb7a46b1b9f70f
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 14:14:03 -0700

    fixed syntax

---
+++ lib/cell.rb
@@ -0,0 +1,23 @@
+class Cell
+
+  attr_reader :ship
+
+  def initialize(placement_parameter)
+    @coordinate = placement_parameter
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def place_ship(ship_parameter)
+    @ship = ship_parameter
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+  end
+
+end
