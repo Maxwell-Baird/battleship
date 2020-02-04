a5ccd0ee710540b96f3f27d1b54c8473c89f2590
“Krista <stadler.krista@gmail..com>
2020-02-04 13:39:37 -0700

    Test ship has been sunk passes

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,34 +6,31 @@ require './lib/ship'
 class ShipTest < Minitest::Test
 
   def test_ship_exist
-    skip
     cruiser = Ship.new("Cruiser", 3)
     assert_instance_of Ship, cruiser
   end
 
-#  def test_ship_has_name
-#    skip
-#    cruiser = Ship.new("Cruiser", 3)
-#    assert_equal "Cruiser", ship.name
-#  end
+  def test_ship_has_name
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", ship.name
+  end
 
   def test_ship_has_length
     cruiser = Ship.new("Cruiser", 3)
     assert_equal 3, cruiser.length
     end
 
-#  def test_ship_health
-#    skip
-#    cruiser = Ship.new("Cruiser", 3)
-#    assert_equal 3, cruiser.health
-#    cruiser.hit
-#    assert_equal 2, cruiser.health
-#    cruiser.hit
-#    assert_equal 1, cruiser.health
-#  end
+  def test_ship_health
+    skip
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.health
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+  end
 
   def test_ship_has_been_sunk
-    skip
     cruiser = Ship.new("Cruiser", 3)
     assert_equal false, cruiser.sunk?
     cruiser.hit
@@ -43,10 +40,10 @@ class ShipTest < Minitest::Test
     assert_equal true, cruiser.sunk?
   end
 
-#  def test_ship_has_been_hit
-#    cruiser = Ship.new("Cruiser", 3)
-#    cruiser.hit
-#    assert_equal 2, cruiser.health
-#  end
+  def test_ship_has_been_hit
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    assert_equal 2, cruiser.health
+  end
 
 end
