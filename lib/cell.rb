70cc19791ffbdb20822ae7e2c8f8bd149aa32313
Maxwell Baird <bairdm71@gmail.com>
2020-02-04 12:18:19 -0700

    initialize the class and allowed for ship to be place

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,12 @@
 class Cell
 
+  attr_reader :ship,
+  def initialize(placement_parameter)
+    @coordinate = placement_parameter
+    @ship = nil
+  end
+
+  def place_ship (ship_parameter)
+    @ship = ship_parameter
+  end
 end
