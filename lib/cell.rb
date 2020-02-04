76b0feee3dcbc85c359138ee3aa838c0b55f9427
“Krista <stadler.krista@gmail..com>
2020-02-04 14:39:32 -0700

    Test if cell is empty passes

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,15 +1,17 @@
 class Cell
 
-  attr_reader :ship
+  attr_reader :ship, :coordinate
 
   def initialize(placement_parameter)
     @coordinate = placement_parameter
     @ship = nil
     @fired_upon = false
+    @cell_empty = true
   end
 
   def place_ship(ship_parameter)
     @ship = ship_parameter
+    @cell_empty = true
   end
 
   def fired_upon?
@@ -18,6 +20,10 @@ class Cell
 
   def fire_upon
     @fired_upon = true
+  end
+
+  def empty?
+    @cell_empty
   end
 
 end
