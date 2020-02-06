8a8b72dd0a673f8bde931f50b94d12783686e83f
“Krista <stadler.krista@gmail..com>
2020-02-06 15:28:35 -0700

    Moved code out of runner to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,25 @@
 class Game
 
-  attr_reader :start_message
+  attr_reader :start_message, :start_another_game
 
   def initialize
     @start_message = "Welcome to BATTLESHIP \n Enter p to play. \n Enter q to quit. \n"
+    @start_another_game = false
+  end
+
+  def new_game_prompt
+    @start_prompt = gets.chomp.downcase
+
+      if @start_prompt == "p"
+
+        @start_another_game = true
+
+        puts "Great! Let's play!"
+      elsif @start_prompt == "q"
+        puts "Goodbye!"
+      else
+        puts "Sorry, I don't understand. Goodbye!"
+      end
   end
 
   def prompt_player_ship_placement
@@ -12,4 +28,7 @@ class Game
     gets.chomp
   end
 
+  def standardize_coordinates
+    require "pry"; binding.pry
+  end
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -5,34 +5,13 @@ game = Game.new
 
 print game.start_message
 
-start_prompt = gets.chomp
-
-  if start_prompt == "p"
-    puts "Great! Let's play!"
-
-    #Computer places their ships
-
-    game.prompt_player_ship_placement
+game.new_game_prompt
 
 
+if game.start_another_game == true
 
+  #Computer Places Ships
 
+  game.prompt_player_ship_placement
 
-
-
-
-
-
-
-
-
-
-
-
-
-  elsif start_prompt == "q"
-    puts "Goodbye!"
-  else
-    puts "Sorry, I don't understand. Goodbye!"
-  end
-#Computer places their ships!
+end
