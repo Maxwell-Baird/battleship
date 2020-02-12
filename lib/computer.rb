dcff7d526a3dd8b9b7cc0bfbd8eead372cf47a7b
Maxwell Baird <bairdm71@gmail.com>
2020-02-12 13:30:37 -0700

    refactor computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -12,57 +12,70 @@ class Computer
   end
 
   def choose_location(ship_parameter)
-    vert_or_horz_choice = ['v']
+    vert_or_horz_choice = ['v','h']
     vert_or_horz = vert_or_horz_choice.sample
-    location = ''
-    cells_location = []
-    if vert_or_horz == 'h' && ship_parameter.length == 3
-      possible_locations_h_3 = ['A1','A2','B1','B2','C1','C2','D1','D2']
-      location = possible_locations_h_3.sample
-      builder_letter = location[0]
-      builder_number = location[1]
-      3.times do
-        holder = builder_letter + builder_number
-        cells_location << holder
-        number = 1 + builder_number.to_i
-        builder_number = number.to_s
-      end
-    elsif vert_or_horz == 'h' && ship_parameter.length == 2
-      possible_locations_h_2 = ['A1','A2','A3','B1','B2','B3',
-                    'C1','C2','C3','D1','D2','D3']
-      location = possible_locations_h_2.sample
-      builder_letter = location[0]
-      builder_number = location[1]
+    @location = ''
+    @cells_location = []
+    if ship_parameter.length == 2
+      size_2(vert_or_horz)
+    elsif ship_parameter.length == 3
+      size_3(vert_or_horz)
+    end
+    @cells_location
+  end
+
+  def size_2(h_or_v)
+    vert_or_horz = h_or_v
+    if vert_or_horz == 'h'
+      possible_locations_h_2 = ['A1','A2','A3','B1','B2','B3','C1','C2','C3','D1','D2','D3']
+      @location = possible_locations_h_2.sample
+      builder_letter = @location[0]
+      builder_number = @location[1]
       2.times do
         holder = builder_letter + builder_number
-        cells_location << holder
+        @cells_location << holder
         number = 1 + builder_number.to_i
         builder_number = number.to_s
       end
-    elsif vert_or_horz == 'v' && ship_parameter.length == 3
-      possible_locations_v_3 = ['A1','A2','A3','A4','B1','B2','B3','B4']
-      location = possible_locations_v_3.sample
-      builder_letter = location[0]
-      builder_number = location[1]
-      3.times do
-        holder = builder_letter + builder_number
-        cells_location << holder
-        letter = builder_letter.next
-        builder_letter = letter
-      end
-    elsif vert_or_horz == 'v' && ship_parameter.length == 2
+    elsif vert_or_horz == 'v'
       possible_locations_v_3 = ['A1','A2','A3','A4','B1','B2','B3','B4','C1','C2','C3','C4']
-      location = possible_locations_v_3.sample
-      builder_letter = location[0]
-      builder_number = location[1]
+      @location = possible_locations_v_3.sample
+      builder_letter = @location[0]
+      builder_number = @location[1]
       2.times do
         holder = builder_letter + builder_number
-        cells_location << holder
+        @cells_location << holder
         letter = builder_letter.next
         builder_letter = letter
       end
     end
-    cells_location
+  end
+
+  def size_3(h_or_v)
+    vert_or_horz = h_or_v
+    if vert_or_horz == 'h'
+      possible_locations_h_3 = ['A1','A2','B1','B2','C1','C2','D1','D2']
+      @location = possible_locations_h_3.sample
+      builder_letter = @location[0]
+      builder_number = @location[1]
+      3.times do
+        holder = builder_letter + builder_number
+        @cells_location << holder
+        number = 1 + builder_number.to_i
+        builder_number = number.to_s
+      end
+    elsif vert_or_horz == 'v'
+      possible_locations_v_3 = ['A1','A2','A3','A4','B1','B2','B3','B4']
+      @location = possible_locations_v_3.sample
+      builder_letter = @location[0]
+      builder_number = @location[1]
+      3.times do
+        holder = builder_letter + builder_number
+        @cells_location << holder
+        letter = builder_letter.next
+        builder_letter = letter
+      end
+    end
   end
 
   def shot_at
@@ -70,6 +83,4 @@ class Computer
     @unfired.delete(shot)
     shot
   end
-
-
 end
